70ac3209092f20b72ba6a7df06c32d5d4a059b2b
Carlos de Manuel Clemente <carlos.demanuel@uclm.es>
2021-03-03 11:05:36 +0100

    ad sh 8

--- ad-hoc_08.sh
+++ ad-hoc_08.sh
@@ -1,3 +1,3 @@
 # Copiar el archivo /usr/bin/hostname del nodo master en el directorio /tmp de los nodos
 
-ansible nodos -m files -a "src='hostname' dest='/tmp/master/user/bin/hostname flat=yes'" 
+ansible nodos -m copy -a "src='/usr/bin/hostname' dest='/tmp'" 
