35dc70b420257456c756fca0a9593124a43df43f
Carlos de Manuel Clemente <carlos.demanuel@uclm.es>
2021-03-03 09:39:44 +0100

    ad-hoc de 5 a 8

--- ad-hoc_07.sh
+++ ad-hoc_07.sh
@@ -1,1 +1,2 @@
 # Instalaremos la paqueteria de vim en todos los nodos
+ansible nodos -m pip -a "name='vim'"
--- ad-hoc_08.sh
+++ ad-hoc_08.sh
@@ -1,2 +1,3 @@
 # Copiar el archivo /usr/bin/hostname del nodo master en el directorio /tmp de los nodos
 
+ansible nodos -m files -a "src='hostname' dest='/tmp/master/user/bin/hostname flat=yes'" 
