c928bfb88a01447b37dfcd00e84ca2091a3fa1d3
Carlos de Manuel Clemente <carlos.demanuel@uclm.es>
2021-03-03 10:11:45 +0100

    Modifcacion 6

--- ad-hoc_06.sh
+++ ad-hoc_06.sh
@@ -1,3 +1,3 @@
 # Añadir la linea "ansible ALL=(ALL:ALL) ALL:NOPASWD" 
 # al archivo /etc/sudoers despues de la linea "root ALL=(ALL:ALL) ALL"
-ansible nodos -m lineinfile -a "path='/etc/sudoers' insertafter='EOF' line='ansible ALL=(ALL:ALL) ALL' state=present"
+ansible nodos -m lineinfile -a "path='/etc/sudoers' insertafter='EOF' line='ansible ALL=(ALL:ALL) ALL:NOPASWD' state=present"
